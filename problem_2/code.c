526050471d9faedbc39080d044115d4a1891345d
ssankalps <ssankalp@protonmail.ch>
2024-07-10 22:19:24 +0530

    Add C code for problem 2.

--- problem_2/code.c
+++ problem_2/code.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+    unsigned sum=0, previous=1, current=1, next=0;
+    next = current + previous;
+    while (next <= 4000000)
+    {
+	if(next % 2 == 0)
+	    sum += next;
+	previous = current;
+	current = next;
+	next = current + previous;
+    }
+    printf("Sum of even valued Fibonacci sequence terms which are less than 4 million): %d\n",sum);
+    return 0;
+}
